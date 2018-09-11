923b0be81172aa0980877a59fa92ac5fa8e1a301
Yunfei Li <dylannnnlee@gmail.com>
2018-09-11 21:28:23 +0200

    Add twitter card meta tag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,25 +10,7 @@ import * as firebase from 'firebase/app';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  constructor(private meta: Meta) {
-    this.meta.addTag({
-      name: 'og:title',
-      content: 'Yunfei is a Senior Frontend Developer and he resident in Amsterdam'
-    });
-    this.meta.addTag({
-      name: 'og:image',
-      content: 'https://www.yunfei.li/assets/img/home-preview.jpg'
-    });
-    this.meta.addTag({
-      name: 'og:description',
-      // tslint:disable-next-line:max-line-length
-      content: `Yunfei is a Senior Frontend Developer and he resident in Amsterdam, the Netherlands. He used to be a creative designer and focus on visual communication. Contact him at dylannnnlee@gmail.com`
-    });
-    this.meta.addTag({
-      name: 'og:url',
-      content: 'https://www.yunfei.li'
-    });
-  }
+  constructor(private meta: Meta) {}
 
   ngOnInit() {
     firebase.initializeApp(environment.firebaseConfig);
